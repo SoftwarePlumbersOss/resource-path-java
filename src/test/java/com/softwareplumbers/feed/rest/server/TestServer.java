f512d010dcb61c038bc02bccfc86cb6f2fef7058
Jonathan Essex <jonathan.essex@softwareplumbers.com>
2020-06-13 15:34:23 -0400

    Use Subject DN from keymanager in place of symbolic username for test.

--- src/test/java/com/softwareplumbers/feed/rest/server/TestServer.java
+++ src/test/java/com/softwareplumbers/feed/rest/server/TestServer.java
@@ -9,11 +9,14 @@ import com.softwareplumbers.feed.FeedExceptions.InvalidPath;
 import com.softwareplumbers.feed.FeedPath;
 import com.softwareplumbers.feed.Message;
 import com.softwareplumbers.feed.TestFeedService;
+import com.softwareplumbers.feed.rest.client.spring.KeyPairs;
 import com.softwareplumbers.feed.test.TestUtils;
+import com.softwareplumbers.keymanager.KeyManager;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -29,11 +32,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ContextConfiguration(classes = { LocalConfig.class }, locations= { "/services.xml" })
 public class TestServer extends TestFeedService  {
     
+    @Autowired KeyManager keyManager;
+    
     @Test 
     public void testUsernamePresentInPostedMessage() throws InvalidPath {
         Message message = TestUtils.generateMessage(TestUtils.randomFeedPath());
         Message result = service.post(FeedPath.ROOT.add("test"), message);
-        assertThat(result.getSender(), equalTo("DEFAULT_SERVICE_ACCOUNT"));
+        String username = keyManager.getCertificate(KeyPairs.DEFAULT_SERVICE_ACCOUNT).getSubjectDN().getName().substring(3);
+        assertThat(result.getSender(), equalTo(username));
     }
 }
 
